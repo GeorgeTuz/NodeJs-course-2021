37f3da68343d6974a9baad8b62df854bc3cf8ede
GeorgeTuz <george_tuz@mail.ru>
2021-05-17 04:54:39 +0300

    feat: add CRUD for users

--- src/resources/users/user.model.js
+++ src/resources/users/user.model.js
@@ -1,4 +1,6 @@
-const uuid = require('uuid');
+const uuidObj = require('uuid');
+
+const uuid = uuidObj.v4;
 
 class User {
   constructor({
--- src/resources/users/user.service.js
+++ src/resources/users/user.service.js
@@ -1,5 +1,9 @@
 const usersRepo = require('./user.memory.repository');
 
 const getAll = () => usersRepo.getAll();
+const getUserById = (id) => usersRepo.getUserById(id);
+const createUser = (user) => usersRepo.createUser(user);
+const updateUser = (id, data) => usersRepo.updateUser(id, data);
+const deleteUser = (id) => usersRepo.deleteUser(id);
 
-module.exports = { getAll };
+module.exports = { getAll, getUserById, createUser, updateUser, deleteUser };
